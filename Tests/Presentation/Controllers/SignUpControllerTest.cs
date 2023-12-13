f836dae870cd555d6d430823602e6cdc9424f4be
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-13 00:30:24 -0300

    test: ensure SignUpController calls Validation with correct values

---
+++ Tests/Presentation/Controllers/SignUpControllerTest.cs
@@ -0,0 +1,34 @@
+﻿using Moq;
+using NUnit.Framework;
+using Presentation.Controllers;
+using Presentation.Protocols;
+
+namespace Tests.Presentation.Controllers
+{
+  [TestFixture]
+  public class SignUpControllerTest
+  {
+    private Mock<Validation> validationMock;
+    private SignUpController sut;
+
+    [SetUp]
+    public void Setup()
+    {
+      validationMock = new Mock<Validation>();
+      sut = new SignUpController(validationMock.Object);
+    }
+
+    [Test]
+    public void ShouldCallValidationWithCorrectValues()
+    {
+      SignUpControllerRequest request = new()
+      {
+        Username = "any_username",
+        Email = "any_email@mail.com",
+        Password = "any_password"
+      };
+      sut.Handle(request);
+      validationMock.Verify(v => v.Validate(It.Is<object>(obj => obj == request)), Times.Once);
+    }
+  }
+}
