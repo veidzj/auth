ce263ef3a248607b4c06113c772a453c57676e70
João Victor Veidz <141789023+veidzj@users.noreply.github.com>
2023-12-13 03:32:22 -0300

    test: ensure SignUpController calls AddAccount with correct values

--- Tests/Presentation/Controllers/SignUpControllerTest.cs
+++ Tests/Presentation/Controllers/SignUpControllerTest.cs
@@ -1,8 +1,11 @@
 ﻿using Bogus;
+using Domain.Usecases;
 using Moq;
 using NUnit.Framework;
 using Presentation.Controllers;
+using Presentation.Implementations;
 using Presentation.Protocols;
+using System.Threading.Tasks;
 using ValidationException = Domain.Errors.ValidationException;
 
 namespace Tests.Presentation.Controllers
@@ -12,13 +15,14 @@ namespace Tests.Presentation.Controllers
   {
     private Faker faker;
     private Mock<IValidation> validationMock;
+    private Mock<IAddAccount> addAccountMock;
     private SignUpController sut;
 
     private SignUpControllerRequest MockRequest()
     {
       SignUpControllerRequest request = new()
       {
-        Username = faker.Internet.UserName(),
+        UserName = faker.Internet.UserName(),
         Email = faker.Internet.Email(),
         Password = faker.Internet.Password()
       };
@@ -30,24 +34,25 @@ namespace Tests.Presentation.Controllers
     {
       faker = new Faker();
       validationMock = new Mock<IValidation>();
-      sut = new SignUpController(validationMock.Object);
+      addAccountMock = new Mock<IAddAccount>();
+      sut = new SignUpController(validationMock.Object, addAccountMock.Object);
     }
 
     [Test]
-    public void ShouldCallValidationWithCorrectValues()
+    public async Task ShouldCallValidationWithCorrectValues()
     {
       SignUpControllerRequest request = MockRequest();
-      sut.Handle(request);
+      await sut.Handle(request);
       validationMock.Verify(v => v.Validate(It.Is<object>(obj => obj == request)), Times.Once);
     }
 
     [Test]
-    public void ShouldReturnBadRequestIfValidationThrows()
+    public async Task ShouldReturnBadRequestIfValidationThrows()
     {
       SignUpControllerRequest request = MockRequest();
       string exceptionMessage = faker.Random.Word();
       validationMock.Setup(v => v.Validate(It.IsAny<object>())).Throws(new ValidationException(exceptionMessage));
-      IResponse response = sut.Handle(request);
+      IResponse response = await sut.Handle(request);
       Assert.Multiple(() =>
       {
         Assert.That(response.StatusCode, Is.EqualTo(400));
@@ -55,6 +60,24 @@ namespace Tests.Presentation.Controllers
         ValidationException? exception = response.Body as ValidationException;
         Assert.That(exception?.Message, Is.EqualTo(exceptionMessage));
       });
+    }
+
+    [Test]
+    public async Task ShouldCallAddAccountWithCorrectValues()
+    {
+      SignUpControllerRequest request = MockRequest();
+      IAddAccountInput addAccountInput = new AddAccountInput()
+      {
+        UserName = request.UserName!,
+        Email = request.Email!,
+        Password = request.Password!
+      };
+      await sut.Handle(request);
+      addAccountMock.Verify(v => v.Add(It.Is<IAddAccountInput>(input =>
+        input.UserName == request.UserName &&
+        input.Email == request.Email &&
+        input.Password == request.Password
+      )), Times.Once);
     }
   }
 }
